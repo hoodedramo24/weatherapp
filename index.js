fbe8d0e12aba1f0667e8a66310be27348140aa68
Omar Alcide <omar.alcide@gmail.com>
2021-10-28 13:17:45 -0400

    intital commit

---
+++ index.js
@@ -0,0 +1,31 @@
+async function getWeather(event){
+    event.preventDefault()
+    const weatherSearchLocation = event.target[0].value
+    const apiKey = "accuweather api"
+    const fetchParams = {
+        method: 'GET'
+    }
+    
+    async function getAPIData(url,params){
+        const response = await fetch(url,params)
+        const data = await response.json()
+        return data
+    }
+
+    locationKeyURL = `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${weatherSearchLocation}`
+    const locationKey = await getAPIData(locationKeyURL,fetchParams)
+    console.log(locationKey[0].Key)
+    currentConditionsURL = `http://dataservice.accuweather.com/currentconditions/v1/${locationKey[0].Key}?apikey=${apiKey}`
+    currentConditions = await getAPIData(currentConditionsURL,fetchParams)
+    console.log(currentConditions[0].Temperature.Imperial.Value)
+    console.log(currentConditions[0].WeatherText)
+
+    temp = currentConditions[0].Temperature.Imperial.Value
+    weather = currentConditions[0].WeatherText
+
+    document.getElementById('currentCondTemp').innerHTML = temp
+    document.getElementById('currentCondWeatherText').innerHTML = weather
+    
+    
+    
+}
